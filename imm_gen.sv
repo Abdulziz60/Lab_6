1ab2e0220899076d0e6d6a470a1efedc8b4dabdb
Abdulziz60 <56158670+Abdulziz60@users.noreply.github.com>
2024-12-25 16:45:03 +0300

    imm_gen_1.1

--- imm_gen.sv
+++ imm_gen.sv
@@ -1,26 +1,26 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/25/2024 10:32:46 AM
-// Design Name: 
-// Module Name: imm_gen
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module imm_gen(
-
+    input  logic [31:0] inst,
+    output logic [31:0] imm 
     );
+    logic [6:0]opcode;
+       
+    assign opcode = inst[6:0];
+    
+    always_comb begin
+    case ( opcode )
+        // I-type    
+        7'b0010011 : imm = {{20{inst[31]}}, inst[31:20]};
+        
+        // S-type    
+        7'b0100011 : imm = {{20{inst[31]}}, inst[31:25], inst[11:7]} ; 
+           
+        // B-type    
+        7'b1100011 : imm = {{19{inst[31]}}, inst[31], inst[7], inst[30:25], inst[11:8], 1'b0} ;    
+        
+        
+        default : imm = 32'b0  ;
+    endcase
+end    
 endmodule
